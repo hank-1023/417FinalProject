7eee390e876ca199a59e3c48e1748b1506367df0
Yuqin Guan <bguan9869@gmail.com>
2019-11-27 18:23:12 -0500

    test65

--- test.py
+++ test.py
@@ -3,16 +3,6 @@ from torrent_function import Torrent
 from contrib.encoding import Decoder
 from contrib.encoding import Encoder
 
-print(open('/Users/yuqinguan/Desktop/FinalProject/417FinalProject/shared.torrent', 'rb').read())
-meta_info = open('/Users/yuqinguan/Desktop/FinalProject/417FinalProject/shared.torrent', 'rb').read()
-L = list(open('/Users/yuqinguan/Desktop/FinalProject/417FinalProject/shared.torrent', 'rb').read())
-K = open('/Users/yuqinguan/Desktop/FinalProject/417FinalProject/shared.torrent', 'rb').read()
-print(L)
-torrent = Decoder(meta_info).decode()
-print(torrent)
-
-L = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-print(L[0:20])
 
 torr = Torrent('/Users/yuqinguan/Desktop/FinalProject/417FinalProject/shared.torrent')
 print(torr.announce())
